9dcf7b9a96caa83160441f694a6867ac6486556f
Artur Diniz Adam <artur.daz@gmail.com>
2018-02-05 18:32:40 -0200

    Ultima aula

--- lib.js
+++ lib.js
@@ -8,6 +8,12 @@
     function transformaDecimalEmTexto(num) {
        return num.toFixed(2).replace(".",",");
     }
+    
+    function mostra(msg){
+        const tag = document.createElement("p")
+        document.body.appendChild(tag)
+        tag.innerHTML = msg
+    }
 
     // // função com parâmetro
     // function prompt(msg){
